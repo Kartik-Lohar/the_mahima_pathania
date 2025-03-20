14fc7d0039324fd1c952321686c1250d8885bb06
kartik Lohar <kartikpanchal1409@gmail.com>
2025-03-20 20:33:06 +0530

    added my project

---
+++ deepseek.py
@@ -0,0 +1,21 @@
+from ollama import chat
+
+response = chat(model="deepseek-r1", messages= [{
+    "role":"user",
+    "content": "what is chatbot ?"
+}])
+
+print(response.message.content)
+
+
+print("")
+
+
+
+
+print("The Mahima Pathania")
+
+
+print("Adglobal queen one and only")
+
+
